89534e714d1906777b6e6d20c958cc8914f9072b
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-08-09 02:28:15 +0300

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+Console.WriteLine();
+
+Console.Write("Enter your name: ");
+string? Client = Console.ReadLine();
+
+Console.Write("Enter the name of the product : ");
+var Product = Console.ReadLine();
+
+Console.Write("Enter the price: ");
+double price = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the name of the street: ");
+string Address = Console.ReadLine();
+
+Console.Write("Enter the street number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine();
+
+Console.WriteLine("Order No 1");
+Console.WriteLine($"Client: {Client}.");
+Console.WriteLine($"Product: {Product}, price {price:0.00} EUR.");
+Console.WriteLine($"Address: {Address}, {number}.");
+
+Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("Enter your name: ");
+ Client = Console.ReadLine();
+
+Console.Write("Enter the name of the product : ");
+ Product = Console.ReadLine();
+
+Console.Write("Enter the price: ");
+ price = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the name of the street: ");
+Address = Console.ReadLine();
+
+Console.Write("Enter the street number: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Order No 2");
+Console.WriteLine($"Client: {Client}.");
+Console.WriteLine($"Product: {Product}, price {price:0.00} EUR.");
+Console.WriteLine($"Address: {Address}, {number}.");
+
+
+Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("Enter your name: ");
+Client = Console.ReadLine();
+
+Console.Write("Enter the name of the product : ");
+Product = Console.ReadLine();
+
+Console.Write("Enter the price: ");
+price = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the name of the street: ");
+Address = Console.ReadLine();
+
+Console.Write("Enter the street number: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine();
+
+Console.WriteLine("Order No 3");
+Console.WriteLine($"Client: {Client}.");
+Console.WriteLine($"Product: {Product}, price {price:0.00} EUR.");
+Console.WriteLine($"Address: {Address}, {number}.");
+
+Console.ReadLine();
+Console.WriteLine();
+
+
+
