6d33dfe9ef129c81c04eb752d895a437729eada3
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-08-09 02:29:04 +0300

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Enter first number");
+double FirstNum = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter action you want to perform");
+char action = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+double SecondNum = double.Parse(Console.ReadLine());
+
+double result = 0;
+
+switch (action)
+{
+    case '+':
+        result = FirstNum + SecondNum;
+    break;
+
+    case '-':
+        result = FirstNum - SecondNum;
+    break;
+
+    case '*':   
+        result = FirstNum * SecondNum;
+    break;
+
+    case '/':
+
+        if (SecondNum == 0)
+
+        {
+            Console.WriteLine("You can not divide by 0!");
+            return;
+        }
+
+        result = FirstNum / SecondNum;
+
+    break;
+
+    default:
+        
+        Console.WriteLine("You entered incorrect action!");
+        return;
+    break;
+}
+
+
+    Console.WriteLine($"{FirstNum} {action} {SecondNum} = {result}");
