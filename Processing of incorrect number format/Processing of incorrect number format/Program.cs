f64f04b4308f2042c71d677e3fa2cc0d88bd56ff
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-08-12 00:20:00 +0300

    Add files via upload

---
+++ Processing of incorrect number format/Processing of incorrect number format/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
+
+try
+{
+    double number = Convert.ToDouble(input);
+    Console.WriteLine($"You entered the number: {number}");
+}
+catch
+{
+  
+    Console.WriteLine("Invalid number format. Please enter a valid number.");
+}
+finally
+{
+   
+    Console.WriteLine("Operation completed.");
+}
