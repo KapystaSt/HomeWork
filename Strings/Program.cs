820fb0c3f7f8bb2547a07d78c5db840256a9e33f
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-09-06 14:39:45 +0300

    Add files via upload

---
+++ Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine();
+        Console.WriteLine("Введіть ім'я та прізвище користувача:");
+        Console.WriteLine();
+        string fullName = Console.ReadLine();
+
+        string[] parts = fullName.Split(' ');
+
+        
+        if (parts.Length >= 2)
+        {
+            string firstName = parts[0];  
+            string lastName = parts[1];   
+
+            
+            if (char.ToUpper(firstName[0]) == char.ToUpper(lastName[0]))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Прізвище починається на ту ж літеру, що і ім'я.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім'я.");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введіть, будь ласка, повне ім'я (ім'я та прізвище).");
+            Console.WriteLine();
+            Main();
+        }
+    }
+}
