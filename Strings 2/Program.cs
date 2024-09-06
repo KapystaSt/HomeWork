cc3671f62cc1aee7c47cf6b6cdaa3f65881b81bf
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-09-06 17:24:33 +0300

    Add files via upload

---
+++ Strings 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        StringBuilder report = new StringBuilder();
+
+        
+        report.AppendLine("Текстовий звіт");
+        report.AppendLine($"Дата: {DateTime.Now.ToShortDateString()}");
+        report.AppendLine("Події:");
+
+
+        
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введіть подію (або 'зупинити' для завершення):");
+            
+            string input = Console.ReadLine();
+
+            
+            if (input.ToLower() == "зупинити")
+                break;
+
+            
+            report.AppendLine($"- {input}");
+        }
+
+        
+        Console.WriteLine("\nГотовий звіт:");
+        Console.WriteLine();
+        Console.WriteLine(report);
+    }
+}
+
