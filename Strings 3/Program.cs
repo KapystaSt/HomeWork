d10aedd4e066f9eda78e8cee955c6c24737a7a57
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-09-08 16:41:14 +0300

    Add files via upload

---
+++ Strings 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть рядок зі словами, розділеними комами:");
+        string input = Console.ReadLine();
+
+        
+        string result = input.Replace(" ", "");
+
+        
+        Console.WriteLine("Рядок без пробілів:");
+        Console.WriteLine(result);
+    }
+}
