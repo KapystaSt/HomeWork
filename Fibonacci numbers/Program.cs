1de41991b7be031bbae08d4be742fef61a6e0911
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-09-11 21:26:43 +0300

    Add files via upload

---
+++ Fibonacci numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Fibonacci_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+           Console.WriteLine("Введіть порядковий номер числа Фібоначчі:");
+           int number = int.Parse(Console.ReadLine());
+
+           int fibonacciNumber = FibonacciCalculator.FibonacciShifted(number);
+
+           Console.WriteLine($"Число Фібоначчі порядку {number}: {fibonacciNumber}");
+
+        }
+
+        
+        static class FibonacciCalculator
+        {
+            
+            public static int FibonacciShifted(int n)
+            {
+                return Fibonacci(n - 1); 
+            }
+
+            
+            public static int Fibonacci(int n)
+            {
+                if (n == 0) return 0;        
+                if (n == 1) return 1;        
+                return Fibonacci(n - 1) + Fibonacci(n - 2);  
+            }
+        }
+
+    }
+}
+
+
