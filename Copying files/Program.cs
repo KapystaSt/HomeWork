bd65adb81930ae8f909004cf85bc87e7b6f0b79c
KapystaSt <165493091+KapystaSt@users.noreply.github.com>
2024-09-22 21:23:34 +0300

    Add files via upload

---
+++ Copying files/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Copying_files
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                
+                Console.Write("Введіть шлях до вихідного файлу: ");
+                string inputFilePath = Console.ReadLine();
+
+                
+                Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+                string outputFilePath = Console.ReadLine();
+
+                
+                if (File.Exists(inputFilePath))
+                {
+                   File.Copy(inputFilePath, outputFilePath, overwrite: true);
+
+                   Console.WriteLine("Файл успішно скопійовано!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Вихідний файл не знайдено. Перевірте шлях і спробуйте ще раз.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Сталася помилка: {ex.Message}");
+            }
+        }
+    }
+
+}
+
